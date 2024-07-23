98b8fc04ac6cf43824813c19b3bd614f4c59fe1a
Worly <tino.vukelic@gmail.com>
2024-07-23 18:18:16 +0200

    feat: add emoji to home screen hehe

--- Appy/Appy-frontend/src/app/pages/home/home.component.ts
+++ Appy/Appy-frontend/src/app/pages/home/home.component.ts
@@ -9,18 +9,52 @@ import { Subscription } from 'rxjs';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit, OnDestroy {
-
   stats?: HomeStats = undefined;
+  emojies: { [key: string]: string } = {};
+
+  numberOfAppointmentsTodayEmoji: { [key: number]: string } = {
+    0: "😢",
+    1: "😕",
+    2: "😐",
+    3: "🙂",
+    4: "😊",
+    5: "😁",
+    6: "😍",
+    7: "🤩",
+  }
 
   private subs: Subscription[] = [];
 
   constructor(private homeService: HomeService) { }
 
   ngOnInit(): void {
-    this.subs.push(this.homeService.getStats().subscribe(s => this.stats = s));
+    this.subs.push(this.homeService.getStats().subscribe(s => {
+      this.stats = s;
+      this.calculateEmojies();
+    }));
   }
 
   ngOnDestroy(): void {
     this.subs.forEach(s => s.unsubscribe());
+  }
+
+  private calculateEmojies() {
+    if (this.stats == null) {
+      this.emojies = {};
+      return;
+    }
+
+    this.emojies = {
+      numberOfAppointmentsCreatedToday: this.getEmojiForNumberOfAppointmentsToday(<number>this.stats.numberOfAppointmentsCreatedToday)
+    }
+  }
+
+  private getEmojiForNumberOfAppointmentsToday(num: number): string {
+    let maxN = Object.entries(this.numberOfAppointmentsTodayEmoji).length - 1;
+
+    if (num < 0) num = 0;
+    if (num > maxN) num = maxN;
+
+    return this.numberOfAppointmentsTodayEmoji[num];
   }
 }
