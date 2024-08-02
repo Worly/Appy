b6a56a6a9ec45504211e06f70f00d6c3bd8c2679
Worly <tino.vukelic@gmail.com>
2024-08-02 17:45:57 +0200

    wip

--- Appy/DTOs/ClientDTO.cs
+++ Appy/DTOs/ClientDTO.cs
@@ -4,8 +4,7 @@
     {
         public int Id { get; set; }
 
-        public string Nickname { get; set; }
-        public string? Name { get; set; }
+        public string Name { get; set; }
         public string? Surname { get; set; }
         public string? PhoneNumber { get; set; }
         public string? Email { get; set; }
---
+++ Appy/Migrations/20240726162341_RemoveClientNickname.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Appy.Migrations
+{
+    public partial class RemoveClientNickname : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            // Set Name to first split of Nickname if Name is null or empty
+            migrationBuilder.Sql("UPDATE \"Clients\" SET \"Name\" = trim(SPLIT_PART(\"Nickname\", ' ', 1)) WHERE \"Name\" IS NULL or trim(\"Name\") = '';");
+
+            // Set Surname to second split of Nickname if Surname is null or empty
+            migrationBuilder.Sql("UPDATE \"Clients\" SET \"Surname\" = nullif(trim(substring(\"Nickname\", (length(split_part(\"Nickname\", ' ', 1))) + 1)), '') WHERE \"Surname\" IS NULL or trim(\"Surname\") = '';");
+
+            migrationBuilder.DropColumn(
+                name: "Nickname",
+                table: "Clients");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Clients",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Clients",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Nickname",
+                table: "Clients",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
