92c94e86a04f75e21639504e6a0a6bd657f034f2
Worly <tino.vukelic@gmail.com>
2024-08-15 16:48:30 +0200

    fix: refresh upcoming unconfirmed on entity change

--- Appy/Appy-frontend/src/app/pages/dashboard/upcoming-unconfirmed/upcoming-unconfirmed.component.ts
+++ Appy/Appy-frontend/src/app/pages/dashboard/upcoming-unconfirmed/upcoming-unconfirmed.component.ts
@@ -4,6 +4,7 @@ import { DashboardService } from '../services/dashboard.service';
 import { Appointment } from 'src/app/models/appointment';
 import { TranslateService } from 'src/app/components/translate/translate.service';
 import { Router } from '@angular/router';
+import { EntityChangeNotifyService } from 'src/app/shared/services/entity-change-notify.service';
 
 @Component({
   selector: 'app-upcoming-unconfirmed',
@@ -21,10 +22,27 @@ export class UpcomingUnconfirmedComponent {
   constructor(
     private router: Router,
     private dashboardService: DashboardService,
-    private translateService: TranslateService
-  ) { }
+    private translateService: TranslateService,
+    private entityChangeNotifyService: EntityChangeNotifyService
+  ) {
+    this.subs.push(
+      ...this.entityChangeNotifyService.for<Appointment>(Appointment.ENTITY_TYPE).subscribeAll({
+        onAdded: () => this.load(),
+        onDeleted: () => this.load(),
+        onUpdated: () => this.load()
+      })
+    );
+  }
 
   ngOnInit(): void {
+    this.load();
+  }
+
+  ngOnDestroy(): void {
+    this.subs.forEach(s => s.unsubscribe());
+  }
+
+  load(): void {
     this.subs.push(this.dashboardService.getUpcomingUnconfirmed().subscribe(a => {
       this.appointments = a;
 
@@ -38,21 +56,17 @@ export class UpcomingUnconfirmedComponent {
     }));
   }
 
-ngOnDestroy(): void {
-  this.subs.forEach(s => s.unsubscribe());
-}
+  openAppointments() {
+    if (this.appointments == null || this.appointments.length == 0)
+      return;
 
-openAppointments() {
-  if (this.appointments == null || this.appointments.length == 0)
-    return;
-
-  this.router.navigate(["appointments"], {
-    queryParams: {
-      date: this.appointments[0].date?.format("YYYY-MM-DD"),
-      filter: JSON.stringify({
-        status: "Unconfirmed"
-      })
-    }
-  });
-}
+    this.router.navigate(["appointments"], {
+      queryParams: {
+        date: this.appointments[0].date?.format("YYYY-MM-DD"),
+        filter: JSON.stringify({
+          status: "Unconfirmed"
+        })
+      }
+    });
+  }
 }
