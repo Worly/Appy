7a3c7f225fe3d0c2bdb55b6fd19a82b762bf2086
Worly <tino.vukelic@gmail.com>
2026-05-29 17:49:38 +0200

    fix: use IHttpClientFactory for InstagramMessagingService (#74)

--- Appy/Program.cs
+++ Appy/Program.cs
@@ -20,7 +20,10 @@ builder.Services.AddDbContext<MainDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("Main")));
 builder.Services.AddSingleton<IJwtService, JwtService>();
 
-builder.Services.AddSingleton<InstagramMessagingService>();
+builder.Services.AddHttpClient<InstagramMessagingService>(client =>
+{
+    client.BaseAddress = new Uri("https://graph.instagram.com/v21.0");
+});
 builder.Services.AddScoped<IMessagingServiceManager, MessagingServiceManager>();
 
 builder.Services.AddScoped<IUserService, UserService>();
