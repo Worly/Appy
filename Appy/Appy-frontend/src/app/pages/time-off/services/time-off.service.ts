0d9920326787a774a93706cdd3b6edf0cd88eafe
Worly <tino.vukelic@gmail.com>
2026-06-14 11:20:14 +0200

    feat(timeoff): invalidate appointment list on time-off change; drop unused getForRange

--- Appy/Appy-frontend/src/app/pages/time-off/services/time-off.service.ts
+++ Appy/Appy-frontend/src/app/pages/time-off/services/time-off.service.ts
@@ -5,23 +5,17 @@ import { appConfig } from "src/app/app.config";
 import { TimeOff } from "src/app/models/time-off";
 import { TimeOffOccurrence, TimeOffOccurrenceDTO } from "src/app/models/time-off-occurrence";
 import { BaseModelService } from "src/app/shared/services/base-model-service";
-import { timeOffKeys } from "src/app/shared/services/data/keys";
+import { appointmentKeys, timeOffKeys } from "src/app/shared/services/data/keys";
 
 @Injectable({ providedIn: "root" })
 export class TimeOffService extends BaseModelService<TimeOff, TimeOff> {
   constructor(injector: Injector) {
-    super(injector, TimeOff.ENTITY_TYPE, TimeOff, TimeOff, timeOffKeys);
+    super(injector, TimeOff.ENTITY_TYPE, TimeOff, TimeOff, timeOffKeys, [appointmentKeys.all]);
   }
 
   public getForDate(date: Dayjs): Observable<TimeOffOccurrence[]> {
     return this.httpClient.get<TimeOffOccurrenceDTO[]>(`${appConfig.apiUrl}${this.controllerName}/getForDate`, {
       params: { date: date.format("YYYY-MM-DD") }
-    }).pipe(map(r => r.map(o => new TimeOffOccurrence(o))));
-  }
-
-  public getForRange(from: Dayjs, to: Dayjs): Observable<TimeOffOccurrence[]> {
-    return this.httpClient.get<TimeOffOccurrenceDTO[]>(`${appConfig.apiUrl}${this.controllerName}/getForRange`, {
-      params: { from: from.format("YYYY-MM-DD"), to: to.format("YYYY-MM-DD") }
     }).pipe(map(r => r.map(o => new TimeOffOccurrence(o))));
   }
 }
